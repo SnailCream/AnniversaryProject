bfca47ac8341486b473766634796b0cd07954be0
carki <94.nogari@gmail.com>
2022-07-14 21:12:41 +0900

    선택한 날짜에 대한 D-day는 나오지만 데이트포메터 형식 적용 안됨 + 계산함수 중복코드제거 필요

--- AnniversaryProject/ViewController.swift
+++ AnniversaryProject/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    @IBOutlet weak var testLabel: UILabel!
+    
     @IBOutlet var d_DayCollection: [UILabel]!//D-day 레이블(좌상단)
-    @IBOutlet var pickedDateLabel: [UILabel]!//선택된 일자(센터)
+    @IBOutlet var pickedDateLabel: [UILabel]!//day + 100,200,300,400된 일자(센터)
     @IBOutlet var imageCollection: [UIImageView]!//이미지뷰 컬렉션
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 \tMM월 dd일"
+        
         imageDesign()
         d_DayDesign()
         pickedDateDesign()
@@ -24,9 +29,8 @@ class ViewController: UIViewController {
         datePicker.preferredDatePickerStyle = .inline // 캘린더형식, v15.5를 사용하기 때문에 .inline 채용
         //datePicker.preferredDatePickerStyle = .wheels //바퀴형 -> 위 아래 슬라이드 / 년도 제외
         //datePicker.preferredDatePickerStyle = .compact // default 설정
+        datePickerClicked(datePicker)
         
-        
-          
     }
     
     func imageDesign() {
@@ -47,13 +51,54 @@ class ViewController: UIViewController {
     }
     
     func pickedDateDesign() {
-        let textArr = ["2021년 \n03월 27일", "2021년 \n07월 06일", "2021년 \n10월 13일", "2022년 \n01월 21일"]
-        
         for i in 0..<pickedDateLabel.count {
-            pickedDateLabel[i].text = textArr[i]
             pickedDateLabel[i].textColor = .white
         }
     }
-
+    
+//    func calculateDays(from date: Date) -> String {
+//        let calendar = Calendar.current
+//        let currentDate = Date()
+//
+//    }
+    
+    @IBAction func datePickerClicked(_ sender: UIDatePicker) {
+//        let pickedDate = sender
+//        let calendar = Calendar.current
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy년 \nMM월 dd일"
+//        var day100 = DateComponents(day: 99)
+//        //day100.day = 100
+//
+//        testLabel.text = formatter.string(from: datePicker.date)
+//
+//        let d100 = calendar.date(byAdding: day100, to: datePicker.date)!
+//
+//        let calDate = calendar.dateComponents([.year, .month, .day], from: datePicker.date, to: d100)
+//
+//
+//        pickedDateLabel[0].text = "\(calDate)"
+//
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 \tMM월 dd일"
+        
+        let day1 = DateComponents(day: 99)
+        let day2 = DateComponents(day: 199)
+        let day3 = DateComponents(day: 299)
+        let day4 = DateComponents(day: 399)
+        
+        let d100 = calendar.date(byAdding: day1, to: datePicker.date)!
+        let d200 = calendar.date(byAdding: day2, to: datePicker.date)!
+        let d300 = calendar.date(byAdding: day3, to: datePicker.date)!
+        let d400 = calendar.date(byAdding: day4, to: datePicker.date)!
+        
+        pickedDateLabel[0].text = "\(d100)"
+        pickedDateLabel[1].text = "\(d200)"
+        pickedDateLabel[2].text = "\(d300)"
+        pickedDateLabel[3].text = "\(d400)"
+        
+    }
+    
 }
 
